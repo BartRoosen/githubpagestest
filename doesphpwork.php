cb6a186fe0f631da7b21e46cdb96902ea1bcfd1d
BartRoosen <bart.roosen@live.be>
2021-04-23 10:32:15 +0200

    added php file

---
+++ doesphpwork.php
@@ -0,0 +1,19 @@
+<?php
+
+$hellyeah = "this works";
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <title>Title</title>
+</head>
+<body>
+<h1><?= $hellyeah ?></h1>
+</body>
+</html>
+
